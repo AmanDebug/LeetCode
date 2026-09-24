a54ed47211961eb1243757795e87d69ae2857f4b
Aman Prasad <p.aman052005@gmail.com>
2026-09-25 00:05:49 +0530

    Time: 1 ms (99.69%) | Memory: 45.1 MB (97.84%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -3,13 +3,11 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int sum = 0;
-            while (num > 0) {
-                sum += num % 10;
+            while (num != 0) {
+                sum += num - (num / 10) * 10; 
                 num /= 10;
             }
-            if (sum == i) {
-                return i;
-            }
+            if (sum == i) return i;
         }
         return -1;
     }
